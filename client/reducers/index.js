1e01f9ea281b18533eeecf25efed8b0e2cb7b1b6
Karivool <evelynlee34@gmail.com>
2016-09-29 05:01:56 -0400

    Modify main reducer index

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -4,6 +4,8 @@ import { routerReducer } from 'react-router-redux';
 import posts from './posts';
 import comments from './comments';
 
+// all reducers run unless otherwise specified when action happens
+
 const rootReducer = combineReducers({
   posts,
   comments,
