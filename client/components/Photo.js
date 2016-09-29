da1ccdcd593414d0f8d1e62336cb5c003f7279d0
Karivool <evelynlee34@gmail.com>
2016-09-29 19:59:43 -0400

    Add comment button

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -23,7 +23,13 @@ const Photo = React.createClass({
         <figcaption>
           <p>{post.captions}</p>
           <div className="control-buttons">
-          
+            <button className="likes">&hearts; {post.likes}</button>
+            <Link className="button" to={'/view/${post.code}'}>
+              <span className="comment-count">
+                <span className="speech-bubble"></span>
+                {comments[post.code] ? comments[post.code].length : 0}
+              </span>
+            </Link>
           </div>
         </figcaption>
       </figure>
