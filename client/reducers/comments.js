f962d33f0c9f27b6e755e3b1e67a5f210f8af914
Karivool <evelynlee34@gmail.com>
2016-09-30 00:17:48 -0400

    Update reducer for comments

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,7 +1,35 @@
 // reducer needs the action and copy of current state
-
-function comments(action, state=[]) {
+function postComments(state = [], action) {
+  switch(action.type){
+    case 'ADD_COMMENT':
+      return [...state, {
+        user: action.author,
+        text: action.comment
+      }];
+    case 'REMOVE_COMMENT':
+      return [
+        ...state.slice(0, action.i),
+        ...state.slice(action.i + 1)
+      ];
+    default:
+      return state;
+  }
   return state;
 }
+
+
+function comments(state=[], action) {
+  if(typeof action.postId !== 'undefined') {
+    return {
+
+      ...state,
+
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
+
+  return state;
+}
+
 
 export default comments;
