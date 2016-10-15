b1b758049dd4dafde1055f615924fb1eba375e51
Karivool <evelynlee34@gmail.com>
2016-10-15 13:24:40 -0400

    Fix spaces

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -13,11 +13,11 @@ import store, { history } from './store';
 
 
 const router = (
-  <Provider store={store}>
-    <Router history={history}>
-      <Route path="/" component={App}>
-        <IndexRoute component={PhotoGrid}></IndexRoute>
-        <Route path="/view/:postId" component={Single}></Route>
+  <Provider store={ store }>
+    <Router history={ history }>
+      <Route path="/" component={ App }>
+        <IndexRoute component={ PhotoGrid }></IndexRoute>
+        <Route path="/view/:postId" component={ Single }></Route>
       </Route>
     </Router>
   </Provider>
